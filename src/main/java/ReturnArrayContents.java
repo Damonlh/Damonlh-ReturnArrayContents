8d471a98aaf11eedf8baddc710f1992ae2a57902
Damonlh <myorderrecipt@gmail.com>
2023-02-27 22:55:54 +0000

    Commit

--- src/main/java/ReturnArrayContents.java
+++ src/main/java/ReturnArrayContents.java
@@ -28,6 +28,14 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+
+        String content = " ";
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            content += nums[i] + " ";
+        }
+        
+        return content;
     }
 }
